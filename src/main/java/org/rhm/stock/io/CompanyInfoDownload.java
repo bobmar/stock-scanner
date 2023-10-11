66745cf09c7002428d347e45257c2be27078fb2b
Bob Mar <bobmar@localhost.localdomain>
2023-10-11 06:30:13 -0700

    Retrieve company profile info

--- src/main/java/org/rhm/stock/io/CompanyInfoDownload.java
+++ src/main/java/org/rhm/stock/io/CompanyInfoDownload.java
@@ -1,11 +1,17 @@
 package org.rhm.stock.io;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -20,7 +26,7 @@ public class CompanyInfoDownload {
     private String ratiosUri;
     @Value(value = "${company.download.prices}")
     private String pricesUri;
-
+    private ObjectMapper mapper = new ObjectMapper();
     private String createUrl(String baseUrl, String endpointUri, String tickerSymbol) {
         String fullUrl = baseUrl + endpointUri;
         return String.format(fullUrl, tickerSymbol, apiKey);
@@ -36,10 +42,20 @@ public class CompanyInfoDownload {
         return url;
     }
 
-    public Map<String, Object> retrieveProfile(String tickerSymbol) {
+    public Map<String, Object> retrieveProfile(String tickerSymbol) throws IOException, InterruptedException {
         Map<String,Object> result = null;
-        URI uri = URI.create(this.createProfileUrl("SMCI"));
+        URI uri = URI.create(this.createProfileUrl(tickerSymbol));
         HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            result = this.mapper.readValue(response.body(), Map.class);
+        }
+        else {
+            result = new HashMap<>();
+            result.put("message", "Failed to retrieve profile");
+            result.put("errorCode", String.valueOf(response.statusCode()));
+        }
         return result;
     }
 }
